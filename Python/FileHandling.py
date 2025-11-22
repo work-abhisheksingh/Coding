542f327fef8a9afb7f197d7da20f3d7617db66a8
Abhishek Singh <work.abhisheksingh@gmail.com>
2025-11-22 12:26:25 +0530

    Removed comments

--- Python/FileHandling.py
+++ Python/FileHandling.py
@@ -1,12 +1,12 @@
 import os
 
 filename = "myfile.txt"
-filepath = os.path.join(os.getcwd(), filename)  # Full path in current folder
+filepath = os.path.join(os.getcwd(), filename)
 
 try:
     user_text = input("Write something to store in the file: ")
 
-    with open(filepath, "a") as file:  # "a" = append mode
+    with open(filepath, "a") as file:
         file.write(user_text + "\n")
 
     print(f"Saved successfully! 📁 Location: {filepath}")
